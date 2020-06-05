bbc3613d0c537d48626701f0dd74892d94bce183
汪心禾 <wangxinhe06@gmail.com>
2020-06-05 17:55:38 +0800

    cURL /node_modules/

--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=$(pwd) && /usr/sbin/httpd
-apk add --no-cache curl && curl -fI 127.0.0.1/
+apk add --no-cache curl && curl -fI 127.0.0.1/ 127.0.0.1/node_modules/jquery/dist/jquery.slim.min.js
